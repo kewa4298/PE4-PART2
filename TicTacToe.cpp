8c7548249f3d2a9f1c5e39d5a54a9ab1d5143654
kewa4298 <kewa4298@colorado.edu>
2022-02-10 16:40:03 -0700

    Adding getPlayerChoice method

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include<tuple>
 class TicTacToe {
     public:
         //NOTE: This is our constructor for a TicTacToe class.
@@ -22,6 +23,26 @@ class TicTacToe {
                 std::cout << std::endl;
                 std::cout << "------------------" << std::endl;
             }
+        }
+
+        std::vector<std::string> getPlayerChoice() {
+            std::vector<std::string> retVec;
+            std::string rowChoice = "";
+            std::string colChoice = "";
+            std::string symbol = "";
+            
+            std::cout<<"Please enter your row choice:";
+            std::cin >> rowChoice;
+            retVec.push_back(rowChoice);
+
+            std::cout<<"Please enter your column choice:";
+            std::cin >> colChoice;
+            retVec.push_back(colChoice);
+
+            std::cout<<"Please enter if you are an X or an O:";
+            std::cin >> symbol;
+            retVec.push_back(symbol);
+            return retVec;
         }
     private:
         std::string board_[3][3];
